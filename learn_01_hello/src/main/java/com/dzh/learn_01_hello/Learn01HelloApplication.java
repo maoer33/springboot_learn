c71858be71ef486039bbc0db4402bda609f22325
戴志豪 <2531371849@qq.com>
2023-01-11 10:20:54 +0800

    Initial commit

---
+++ learn_01_hello/src/main/java/com/dzh/learn_01_hello/Learn01HelloApplication.java
@@ -0,0 +1,13 @@
+package com.dzh.learn_01_hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learn01HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Learn01HelloApplication.class, args);
+    }
+
+}
---
+++ learn_01_hello/src/test/java/com/dzh/learn_01_hello/Learn01HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzh.learn_01_hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learn01HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
