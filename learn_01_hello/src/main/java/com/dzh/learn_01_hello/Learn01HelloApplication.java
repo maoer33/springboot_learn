2a64a056cd029b9b5733aafb8ad13ff8bd6fe04f
戴志豪 <2531371849@qq.com>
2023-01-12 13:51:33 +0800

    helloworld

--- learn_01_hello/src/main/java/com/dzh/learn_01_hello/Learn01HelloApplication.java
+++ learn_01_hello/src/main/java/com/dzh/learn_01_hello/Learn01HelloApplication.java
@@ -2,12 +2,21 @@ package com.dzh.learn_01_hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Learn01HelloApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Learn01HelloApplication.class, args);
+        // 返回ioc容器
+        ConfigurableApplicationContext run = SpringApplication.run(Learn01HelloApplication.class, args);
+
+        // 获取所有默认装载的配置，也就是自动配置好的
+        for (String beanDefinitionName : run.getBeanDefinitionNames()) {
+            System.out.println(beanDefinitionName);
+        }
+
+
     }
 
 }
---
+++ learn_01_hello/src/main/java/com/dzh/learn_01_hello/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.dzh.learn_01_hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author 戴志豪
+ * @date 2023/1/11 10:31
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public @ResponseBody Map<String,Object> hello(){
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("msg","helloSpringboot");
+        return map;
+    }
+
+}
